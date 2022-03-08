a54dbba80d35f8fd05b05665941145bb7c81e00f
Slavuk <anslava00@mail.ru>
2022-03-09 09:50:10 +1000

    PushLastGaus

--- lab2/OptimalExp.py
+++ lab2/OptimalExp.py
@@ -27,17 +27,41 @@ class ChooseGause:
 
 
     def CalcRoot(self):
-        print(self.M)
-        print("some")
-        print("add some")
+        #A = self.M[0][0]
+        #for i in range(0, self.N + 1):
+        #    self.M[0][i] /= A
+        for i in range(0, self.N):
+            self.PrintMatrix()
+            for d in range(0, i):
+                DivElement = self.M[i][d]
+                for j in range(0, self.N + 1):
+                    self.M[i][j] -= self.M[d][j] * DivElement
+            DiagElement = self.M[i][i]
+            for j in range(i, self.N + 1):
+                self.M[i][j] /= DiagElement
+            for u in range(0, i):
+                DivElement = self.M[u][i]
+                for j in range(0, self.N + 1):
+                    self.M[u][j] -= self.M[i][j] * DivElement
+        self.PrintMatrix()
+        return [[i, self.M[i][-1]]for i in range(0, self.N)]
+
+        
+    def PrintMatrix(self):
+        print()
+        for i in range(0, self.N):
+            for j in range(0, self.N + 1):
+                print(self.M[i][j], end=" ")
+            print()
+        print()
 
 def main():
     File = ["Input3n3Ch", "Input4n4Ch", "Input3n3Op"]
-    Matrix = ChooseGause('O:\\Lesson\\FileLesson\\ВЫЧМАТ\\lab2\\' + File[2])
+    Matrix = ChooseGause('O:\\Lesson\\FileLesson\\ВЫЧМАТ\\lab2\\' + File[0])
     Root = Matrix.CalcRoot()
 
-    #for i in range(0, len(Root)):
-    #    print("x" + str(Root[i][0] + 1) + " = " + str(Root[i][1]))
+    for i in range(0, len(Root)):
+        print("x" + str(Root[i][0] + 1) + " = " + str(Root[i][1]))
 
 if __name__ == "__main__":
     main()
